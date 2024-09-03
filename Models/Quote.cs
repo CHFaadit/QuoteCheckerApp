f174bde688089f668721dfe7ad11d77c1bf4b1aa
CHFaadit <149717781+CHFaadit@users.noreply.github.com>
2024-09-02 21:15:37 -0400

    Add project files.

---
+++ Models/Quote.cs
@@ -0,0 +1,16 @@
+﻿namespace QuoteApp.Models
+{
+    public class Quote
+    {
+
+        public int Id { get; set; }
+        public string QuoteQuestion { get; set; }
+        
+        public string QuoteReveal { get; set; }
+
+        public Quote()
+        {
+            
+        }
+    }
+}
